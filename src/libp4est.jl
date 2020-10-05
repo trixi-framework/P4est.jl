895e4f1f115994376240cb14577c9478bdca48e7
Michael Schlottke-Lakemper <michael@sloede.com>
2020-10-05 12:27:49 +0200

    Forward declare FILE and va_list

--- src/libp4est.jl
+++ src/libp4est.jl
@@ -1,14 +1,18 @@
 @reexport baremodule LibP4est
   using CBinding: 𝐣𝐥
   
+  # Introduce standard integer types
   const size_t  = 𝐣𝐥.Csize_t
   const ssize_t = 𝐣𝐥.Cssize_t
   const int8_t  = 𝐣𝐥.Int8
   const int16_t = 𝐣𝐥.Int16
   const int32_t = 𝐣𝐥.Int32
   const int64_t = 𝐣𝐥.Int64
-  const FILE = Cvoid # FIXME: Use proper C type
-  const va_list = Cvoid # FIXME: Use proper C type
+
+  # Forward declare standard library types
+  𝐣𝐥.@cstruct FILE
+  𝐣𝐥.@cstruct va_list
+
   𝐣𝐥.Base.include((𝐣𝐥.@__MODULE__), 𝐣𝐥.joinpath(𝐣𝐥.dirname(𝐣𝐥.@__DIR__), "deps", "libp4est.jl"))
 
   """
