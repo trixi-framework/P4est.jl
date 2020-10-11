8cd5634076ca6c96c2e3af1311c63564af53188c
Hendrik Ranocha <mail@ranocha.de>
2020-10-11 10:39:49 +0200

    build P4est.jl with custom non-MPI build of libp4est successfully

--- deps/build.jl
+++ deps/build.jl
@@ -14,28 +14,12 @@ end
 
 config = TOML.parsefile(config_toml)
 
-# P4est.toml has 4 keys
-#  binary   = "" (default) | "system"
-#  path     = "" (default) | top-level directory location
-#  library  = "" (default) | library name/path | list of library names/paths
-#  include  = "" (default) | include name/path | list of include names/paths
+# P4est.toml has 2 keys
+#  library  = "" (default) | library name/path
+#  include  = "" (default) | include name/path
 
 
 # Step 1: Check environment variables and update preferences accordingly
-if haskey(ENV, "JULIA_P4EST_BINARY")
-	config["binary"] = ENV["JULIA_P4EST_BINARY"]
-elseif haskey(ENV, "JULIA_P4EST_LIBRARY") || haskey(ENV, "JULIA_P4EST_PATH")
-	config["binary"] = "system"
-else
-	config["binary"] = ""
-end
-
-if haskey(ENV, "JULIA_P4EST_PATH")
-	config["path"] = ENV["JULIA_P4EST_PATH"]
-else
-	config["path"] = ""
-end
-
 if haskey(ENV, "JULIA_P4EST_LIBRARY")
 	config["library"] = ENV["JULIA_P4EST_LIBRARY"]
 else
@@ -48,12 +32,6 @@ else
 	config["include"] = ""
 end
 
-if haskey(ENV, "JULIA_P4EST_MPI_INCLUDE")
-	config["mpi_include"] = ENV["JULIA_P4EST_MPI_INCLUDE"]
-else
-	config["mpi_include"] = ""
-end
-
 
 open(config_toml, "w") do io
 	TOML.print(io, config)
@@ -62,12 +40,6 @@ end
 
 
 # Step 2: Choose the library according to the settings
-if config["binary"] == ""
-	# TODO
-elseif config["binary"] == "system"
-	# TODO
-end
-
 if isempty(config["library"])
 	println("Use p4est library provided by P4est_jll")
 	p4est_library = P4est_jll.libp4est_path
@@ -89,26 +61,12 @@ else
 	push!(include_directories, config["include"])
 end
 
-if isempty(config["mpi_include"])
-	# println("Use MPI include path provided by P4est_jll")
-	# push!(include_directories,
-	# 			joinpath(dirname(dirname(P4est_jll.libp4est_path)), "include"))
-else
-	println("Use custom MPI include path $(config["mpi_include"])")
-	push!(include_directories, config["mpi_include"])
-end
-
 
 
 # Step 4: Generate binding using the include path according to the settings
 
 # Manually set header files to consider
-# hdrs = ["sc.h", "p4est.h", "p6est.h", "p8est.h"]
-hdrs = ["p4est.h", "p6est.h", "p8est.h"]
-# if !isempty(config["mpi_include"])
-# 	push!(hdrs, "mpi.h")
-# 	push!(hdrs, "mpi-ext.h")
-# end
+hdrs = ["p4est.h", "p4est_extended.h", "p8est.h", "p8est_extended.h"]
 
 # Convert symbols in header
 include_args = String[]
@@ -116,7 +74,6 @@ include_args = String[]
 for dir in include_directories
 	append!(include_args, ("-I", dir))
 end
-@show include_args
 cvts = convert_headers(hdrs, args=include_args) do cursor
 	header = CodeLocation(cursor).file
 	name   = string(cursor)
@@ -126,9 +83,9 @@ cvts = convert_headers(hdrs, args=include_args) do cursor
 	if !(filename in hdrs ||
 		   startswith(filename, "p4est_") ||
 		   startswith(filename, "p6est_") ||
-		   startswith(filename, "p8est_") )#||
-			#  startswith(filename, "sc_") )#||
-			#  filename == "sc.h" )
+		   startswith(filename, "p8est_") ||
+			 startswith(filename, "sc_") ||
+			 filename == "sc.h" )
 		return false
 	end
 	@show filename
