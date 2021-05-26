497a272281a45da8911ac667a2869d77a6e361da
Michael Schlottke-Lakemper <michael@sloede.com>
2021-05-26 13:44:52 +0200

    Fix obvious error in build script

--- .ci_install_p4est.sh
+++ .ci_install_p4est.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-$P4EST_RELEASE="2.2"
+P4EST_RELEASE="2.2"
 
 if [ "${P4EST_TEST}" = "P4EST_JL_NON_MPIL_PRE_GENERATED_BINDINGS" ]; then
   echo "Found 'P4EST_TEST=${P4EST_TEST}'. Nothing to do here."
