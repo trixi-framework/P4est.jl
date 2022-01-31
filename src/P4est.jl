b1021cf3a3f9a4302500f6edfa49f8848269548b
Lars Christmann <lars@l12n.eu>
2022-01-31 08:33:56 +0100

    Add function for checking if p4est uses MPI

--- src/P4est.jl
+++ src/P4est.jl
@@ -5,4 +5,6 @@ using Reexport: @reexport
 
 include("libp4est.jl")
 
+uses_mpi() = isdefined(@__MODULE__, :P4EST_ENABLE_MPI)
+
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,8 @@
 using Test
 using P4est
 
+const P4EST_TEST = get(ENV, "P4EST_TEST", "")
+
 @testset "P4est.jl tests" begin
   @testset "p4est_connectivity_new_periodic" begin
     @test p4est_connectivity_new_periodic() isa Ptr{p4est_connectivity}
@@ -14,5 +16,17 @@ using P4est
   @testset "unsafe_wrap" begin
     obj = unsafe_wrap(connectivity)
     @test obj.num_vertices == 4
+  end
+  
+  @testset "uses_mpi" begin
+    if P4EST_TEST == "P4EST_JLL_USES_MPI_PRE_GENERATED_BINDINGS"
+      @test P4est.uses_mpi() == true
+    elseif P4EST_TEST == "P4EST_JLL_USES_MPI"
+      @test P4est.uses_mpi() == true
+    elseif P4EST_TEST == "P4EST_CUSTOM_NON_MPI"
+      @test P4est.uses_mpi() == false
+    elseif P4EST_TEST == "P4EST_CUSTOM_USES_MPI"
+      @test P4est.uses_mpi() == true
+    end
   end
 end
