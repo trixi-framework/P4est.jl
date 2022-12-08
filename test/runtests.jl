0402aea7bb3bbd3266bff6d65a7f0a207c2bd25c
Hendrik Ranocha <mail@ranocha.de>
2022-12-08 07:33:12 +0100

    add test of p4est_qcoord_to_vertex

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,23 +1,49 @@
 using Test
+
+using MPI: MPI
 using P4est
 
 const P4EST_TEST = get(ENV, "P4EST_TEST", "")
 
+
+# This belongs to the testset "p4est_qcoord_to_vertex" below. However,
+# it looks like we need to define the functions outside of `@testset`
+# to make `@cfunction` work.
+function iter_volume_for_p4est_qcoord_to_vertex(info::Ptr{p4est_iter_volume_info_t}, user_data)
+  info = unsafe_load(info)
+  p4est = unsafe_load(info.p4est)
+  quad = unsafe_load(info.quad)
+
+  vxyz = Array{Float64}(undef, 3)
+  p4est_qcoord_to_vertex(p4est.connectivity, info.treeid, quad.x, quad.y, vxyz)
+
+  println(vxyz)
+  return nothing
+end
+
+
 @testset "P4est.jl tests" begin
+  @test_nowarn MPI.Init()
+
   @testset "p4est_connectivity_new_periodic" begin
+    connectivity = @test_nowarn p4est_connectivity_new_periodic()
     @test p4est_connectivity_new_periodic() isa Ptr{p4est_connectivity}
+    @test_nowarn p4est_connectivity_destroy(connectivity)
   end
 
-  connectivity = p4est_connectivity_new_periodic()
   @testset "p4est_connectivity_is_valid" begin
+    connectivity = @test_nowarn p4est_connectivity_new_periodic()
     @test p4est_connectivity_is_valid(connectivity) == 1
+    @test_nowarn p4est_connectivity_destroy(connectivity)
   end
 
-  @testset "unsafe_wrap" begin
-    obj = unsafe_wrap(connectivity)
-    @test obj.num_vertices == 4
+  @testset "unsafe_load" begin
+    connectivity = @test_nowarn p4est_connectivity_new_periodic()
+    _connectivity = unsafe_load(connectivity)
+    @test _connectivity.num_vertices == 4
+    @test_nowarn p4est_connectivity_destroy(connectivity)
   end
-  
+
   @testset "uses_mpi" begin
     if P4EST_TEST == "P4EST_JLL_USES_MPI_PRE_GENERATED_BINDINGS"
       @test P4est.uses_mpi() == true
@@ -28,5 +54,14 @@ const P4EST_TEST = get(ENV, "P4EST_TEST", "")
     elseif P4EST_TEST == "P4EST_CUSTOM_USES_MPI"
       @test P4est.uses_mpi() == true
     end
+  end
+
+  @testset "p4est_qcoord_to_vertex" begin
+    iter_volume_c = @cfunction(iter_volume_for_p4est_qcoord_to_vertex, Cvoid, (Ptr{p4est_iter_volume_info_t}, Ptr{Cvoid}))
+    connectivity = @test_nowarn p4est_connectivity_new_brick(2, 2, 0, 0)
+    p4est = @test_nowarn p4est_new_ext(MPI.COMM_WORLD, connectivity, 0, 0, true, 0, C_NULL, C_NULL)
+    p4est_iterate(p4est, C_NULL, C_NULL, iter_volume_c, C_NULL, C_NULL)
+    @test_nowarn p4est_destroy(p4est)
+    @test_nowarn p4est_connectivity_destroy(connectivity)
   end
 end
