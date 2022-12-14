a65de3d296ee1b0d8d57334fdeac42afdab4a19e
Hendrik Ranocha <mail@ranocha.de>
2022-12-14 17:09:28 +0100

    debugging docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,13 +1,16 @@
-using Documenter
-import Pkg
 
 # If we want to build the docs locally, add the parent folder to the
 # load path so that we can use the current development version of P4est.jl.
 # See also https://github.com/trixi-framework/Trixi.jl/issues/668
-if (get(ENV, "CI", nothing) != "true") && (get(ENV, "P4EST_DOC_DEFAULT_ENVIRONMENT", nothing) != "true")
+if (get(ENV, "CI", nothing) != "true") && (get(ENV, "JULIA_P4EST_DOC_DEFAULT_ENVIRONMENT", nothing) != "true")
     push!(LOAD_PATH, dirname(@__DIR__))
 end
 
+import Pkg
+Pkg.activate(@__DIR__)
+Pkg.instantiate()
+
+using Documenter
 using P4est
 
 # Define module-wide setups such that the respective modules are available in doctests
@@ -18,7 +21,7 @@ makedocs(
   # Specify modules for which docstrings should be shown
   modules = [P4est],
   # Set sitename to P4est
-  sitename="P4est.jl",
+  sitename = "P4est.jl",
   # Provide additional formatting options
   format = Documenter.HTML(
     # Disable pretty URLs during manual testing
@@ -26,7 +29,7 @@ makedocs(
     # Explicitly add favicon as asset
     # assets = ["assets/favicon.ico"],
     # Set canonical URL to GitHub pages URL
-    canonical = "https://trixi-framework.github.io/P4est.jl/dev"
+    canonical = "https://trixi-framework.github.io/P4est.jl/stable"
   ),
   # Explicitly specify documentation structure
   pages = [
