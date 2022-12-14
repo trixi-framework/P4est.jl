b48b84e293c95649928ab927954c37343c85bc34
Hendrik Ranocha <mail@ranocha.de>
2022-12-14 05:28:31 +0100

    prepare new CI setup

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,41 @@
 using Test
 
+# Configure the test setup based on environment variables set in CI.
+# We test the following combinations set in `P4EST_TEST`:
+# - `P4EST_JLL_MPI_DEFAULT`
+# - `P4EST_JLL_MPI_CUSTOM`
+# - `P4EST_CUSTOM_MPI_DEFAULT`
+# - `P4EST_CUSTOM_MPI_CUSTOM`
+# For CI runs testing a custom installation of `p4est`, the path to the
+# `p4est` library (`libp4est.so` on Linux) has to be set via the environment
+# variable `P4EST_TEST_LIBP4EST`.
+#
+# First, we get the settings and remove all local preference configurations
+# that may still exist.
+const P4EST_TEST = get(ENV, "P4EST_TEST", "P4EST_JLL_MPI_DEFAULT")
+const P4EST_TEST_LIBP4EST = get(ENV, "P4EST_TEST_LIBP4EST", "")
+@test_nowarn rm(joinpath(@__DIR__, "LocalPreferences.toml"), force = true)
+
+# Next, we configure MPI.jl appropriately.
+@static if P4EST_TEST in ("P4EST_JLL_MPI_CUSTOM", "P4EST_CUSTOM_MPI_CUSTOM")
+  import MPIPreferences
+  MPIPreferences.use_system_binary()
+end
+
+# Finally, we configure P4est.jl as desired.
+@static if P4EST_TEST in ("P4EST_CUSTOM_MPI_DEFAULT", "P4EST_CUSTOM_MPI_CUSTOM")
+  import UUIDs, Preferences
+  Preferences.set_preferences!(
+    UUIDs.UUID("7d669430-f675-4ae7-b43e-fab78ec5a902"), # UUID of P4est.jl
+    "libp4est" => P4EST_TEST_LIBP4EST, force = true)
+end
+
+@info "P4est.jl tests configured" P4EST_TEST P4EST_TEST_LIBP4EST
+
+# From here on, all packages should be configured as desired and we can load
+# everything and perform the tests.
 using MPI: MPI
 using P4est
-
-const P4EST_TEST = get(ENV, "P4EST_TEST", "")
 
 
 # This belongs to the testset "p4est_qcoord_to_vertex" below. However,
@@ -25,6 +57,10 @@ end
 @testset "P4est.jl tests" begin
   @test_nowarn MPI.Init()
 
+  @testset "uses_mpi" begin
+    @test P4est.uses_mpi() == true
+  end
+
   @testset "p4est_connectivity_new_periodic" begin
     connectivity = @test_nowarn p4est_connectivity_new_periodic()
     @test p4est_connectivity_new_periodic() isa Ptr{p4est_connectivity}
@@ -42,18 +78,6 @@ end
     _connectivity = unsafe_load(connectivity)
     @test _connectivity.num_vertices == 4
     @test_nowarn p4est_connectivity_destroy(connectivity)
-  end
-
-  @testset "uses_mpi" begin
-    if P4EST_TEST == "P4EST_JLL_USES_MPI_PRE_GENERATED_BINDINGS"
-      @test P4est.uses_mpi() == true
-    elseif P4EST_TEST == "P4EST_JLL_USES_MPI"
-      @test P4est.uses_mpi() == true
-    elseif P4EST_TEST == "P4EST_CUSTOM_NON_MPI"
-      @test P4est.uses_mpi() == false
-    elseif P4EST_TEST == "P4EST_CUSTOM_USES_MPI"
-      @test P4est.uses_mpi() == true
-    end
   end
 
   @testset "p4est_qcoord_to_vertex" begin
