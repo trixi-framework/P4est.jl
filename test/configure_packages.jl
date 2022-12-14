f5e4bee21fb15f0e90583e1c21c97ce1a45a7db8
Hendrik Ranocha <mail@ranocha.de>
2022-12-14 06:16:26 +0100

    try to fix CI

---
+++ test/configure_packages.jl
@@ -0,0 +1,35 @@
+using Pkg
+Pkg.activate(@__DIR__)
+Pkg.instantiate()
+
+# Configure the test setup based on environment variables set in CI.
+# We test the following combinations set in `P4EST_TEST`:
+# - `P4EST_JLL_MPI_DEFAULT`
+# - `P4EST_JLL_MPI_CUSTOM`
+# - `P4EST_CUSTOM_MPI_DEFAULT`
+# - `P4EST_CUSTOM_MPI_CUSTOM`
+# For CI runs testing a custom installation of `p4est`, the path to the
+# `p4est` library (`libp4est.so` on Linux) has to be set via the environment
+# variable `P4EST_TEST_LIBP4EST`.
+#
+# First, we get the settings and remove all local preference configurations
+# that may still exist.
+const P4EST_TEST = get(ENV, "P4EST_TEST", "P4EST_JLL_MPI_DEFAULT")
+const P4EST_TEST_LIBP4EST = get(ENV, "P4EST_TEST_LIBP4EST", "")
+@test_nowarn rm(joinpath(@__DIR__, "LocalPreferences.toml"), force = true)
+
+# Next, we configure MPI.jl appropriately.
+@static if P4EST_TEST in ("P4EST_JLL_MPI_CUSTOM", "P4EST_CUSTOM_MPI_CUSTOM")
+  import MPIPreferences
+  MPIPreferences.use_system_binary()
+end
+
+# Finally, we configure P4est.jl as desired.
+@static if P4EST_TEST in ("P4EST_CUSTOM_MPI_DEFAULT", "P4EST_CUSTOM_MPI_CUSTOM")
+  import UUIDs, Preferences
+  Preferences.set_preferences!(
+    UUIDs.UUID("7d669430-f675-4ae7-b43e-fab78ec5a902"), # UUID of P4est.jl
+    "libp4est" => P4EST_TEST_LIBP4EST, force = true)
+end
+
+@info "P4est.jl tests configured" P4EST_TEST P4EST_TEST_LIBP4EST
