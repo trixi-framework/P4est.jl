76af89acc0a506ad0a4f906668c3e155a757629b
Joshua Lampert <joshua.lampert@tuhh.de>
2022-12-20 10:34:23 +0100

    correct coarsen_fn

--- test/tests_basic.jl
+++ test/tests_basic.jl
@@ -30,13 +30,8 @@ function refine_fn(p4est, which_tree, quadrant)
   end
 end
 
-function coarsen_fn(p4est, which_tree, quadrant)
-  quadrant_obj = unsafe_load(quadrant)
-  if quadrant_obj.level > 0
-    return Cint(1)
-  else
-    return Cint(0)
-  end
+function coarsen_fn(p4est, which_tree, quadrants)
+  return Cint(1)
 end
 
 # This function belongs to the testset "p4est_balance" below
@@ -117,7 +112,7 @@ end
     @test unsafe_load(p4est).global_num_quadrants == 16
     coarsen_fn_c = @cfunction(coarsen_fn, Cint, (Ptr{p4est_t}, Ptr{p4est_topidx_t}, Ptr{p4est_quadrant_t}))
     @test_nowarn p4est_coarsen(p4est, true, coarsen_fn_c, C_NULL)
-    @info unsafe_load(p4est).global_num_quadrants
+    @test unsafe_load(p4est).global_num_quadrants == 1
     @test_nowarn p4est_destroy(p4est)
     @test_nowarn p4est_connectivity_destroy(connectivity)
   end
