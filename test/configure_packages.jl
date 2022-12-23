856902a231864d54cf01c269563d009d08669ed6
Hendrik Ranocha <mail@ranocha.de>
2022-12-23 11:05:36 +0100

    remove P4EST_CUSTOM_MPI_DEFAULT

--- test/configure_packages.jl
+++ test/configure_packages.jl
@@ -5,7 +5,6 @@ Pkg.instantiate()
 # Configure the test setup based on environment variables set in CI.
 # We test the following combinations set in `JULIA_P4EST_TEST`:
 # - `P4EST_JLL_MPI_DEFAULT`
-# - `P4EST_CUSTOM_MPI_DEFAULT`
 # - `P4EST_CUSTOM_MPI_CUSTOM`
 # For CI runs testing a custom installation of `p4est`, the path to the
 # `p4est` library (`libp4est.so` on Linux) has to be set via the environment
@@ -24,7 +23,7 @@ rm(joinpath(dirname(@__DIR__), "LocalPreferences.toml"), force = true)
 end
 
 # Finally, we configure P4est.jl as desired.
-@static if JULIA_P4EST_TEST in ("P4EST_CUSTOM_MPI_DEFAULT", "P4EST_CUSTOM_MPI_CUSTOM")
+@static if JULIA_P4EST_TEST == "P4EST_CUSTOM_MPI_CUSTOM"
   import UUIDs, Preferences
   Preferences.set_preferences!(
     UUIDs.UUID("7d669430-f675-4ae7-b43e-fab78ec5a902"), # UUID of P4est.jl
