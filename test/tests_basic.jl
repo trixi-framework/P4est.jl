0cbad1b20ceb66a2c0ff063ef05619811c307d53
Joshua Lampert <51029046+JoshuaLampert@users.noreply.github.com>
2023-05-08 20:31:13 +0200

    Allow `unsafe_wrap`ping `PointerWrapper`s to vectors (#82)
    * allow unsafe_wrapping PointerWrappers to vectors
    
    * fix order in tests
    
    * Update tests_basic.jl

--- test/tests_basic.jl
+++ test/tests_basic.jl
@@ -85,17 +85,23 @@ end
 
   # `unsafe_wrap`ping a `PointerWrapper`
   n_vertices::Int = connectivity_pw.num_vertices[]
-  vertices = @test_nowarn unsafe_wrap(Array, connectivity_pw.vertices, (3, n_vertices))
-  @test vertices isa Array{Float64, 2}
+  # wrapping matrices
+  vertices_matrix = @test_nowarn unsafe_wrap(Array, connectivity_pw.vertices, (3, n_vertices))
+  @test vertices_matrix isa Array{Float64, 2}
   @test unsafe_wrap(Array{Float64}, connectivity_pw.vertices, (3, n_vertices)) isa Array{Float64, 2}
   @test unsafe_wrap(Array{Float64, 2}, connectivity_pw.vertices, (3, n_vertices)) isa Array{Float64, 2}
 
-  @test size(vertices) == (3, n_vertices)
-  @test vertices[1, 1] == connectivity_pw.vertices[1] == 0.0
-  @test_nowarn vertices[1, 1] = 1.0
-  @test vertices[1, 1] == connectivity_pw.vertices[1] == 1.0
+  @test size(vertices_matrix) == (3, n_vertices)
+  @test vertices_matrix[1, 1] == connectivity_pw.vertices[1] == 0.0
+  @test_nowarn vertices_matrix[1, 1] = 1.0
+  @test vertices_matrix[1, 1] == connectivity_pw.vertices[1] == 1.0
   @test_nowarn connectivity_pw.vertices[1] = 2.0
-  @test vertices[1, 1] == connectivity_pw.vertices[1] == 2.0
+  @test vertices_matrix[1, 1] == connectivity_pw.vertices[1] == 2.0
+  # wrapping vectors
+  vertices_vector = @test_nowarn unsafe_wrap(Array, connectivity_pw.vertices, 3 * n_vertices)
+  @test vertices_vector isa Array{Float64, 1}
+  @test unsafe_wrap(Array{Float64}, connectivity_pw.vertices, 3 * n_vertices) isa Array{Float64, 1}
+  @test unsafe_wrap(Array{Float64, 1}, connectivity_pw.vertices, 3 * n_vertices) isa Array{Float64, 1}
 
   @test_nowarn p4est_destroy(p4est_pw)
   @test_nowarn p4est_connectivity_destroy(connectivity_pw)
