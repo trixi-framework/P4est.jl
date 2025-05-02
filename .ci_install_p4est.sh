07e278dd42f4ca126e84342c26a7140938dc5d68
Joshua Lampert <51029046+JoshuaLampert@users.noreply.github.com>
2025-05-02 17:10:28 +0200

    Debug custom p4est installation CI (#145)
    * add LIBS argument
    
    * install libblas-dev
    
    * also install lapack

--- .ci_install_p4est.sh
+++ .ci_install_p4est.sh
@@ -21,6 +21,7 @@ if [ "${JULIA_P4EST_TEST}" = "P4EST_CUSTOM_MPI_CUSTOM" ]; then
   export CXX=mpicxx
   export FC=mpif90
   export F77=mpif77
+  sudo apt-get install -y libblas-dev liblapack-dev
   $P4EST_TMP/p4est-${P4EST_RELEASE}/configure --prefix=$P4EST_TMP/prefix --enable-mpi --enable-debug
   make -j 2
   make install
