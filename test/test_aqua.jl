c42261e34cd721f58e6ce8e58d139f67e4e962ff
Joshua Lampert <51029046+JoshuaLampert@users.noreply.github.com>
2026-09-18 15:22:57 +0000

    Set minimum julia version to v1.10 (#183)
    * set minimum julia version to v1.10
    
    * add ExplicitImport.jl tests
    
    * format
    
    * disable some failing ExplicitImports.jl tests
    
    * add NEWS entry

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -1,11 +1,22 @@
 module TestAqua
 
-using Aqua
+using Aqua: Aqua
+using ExplicitImports: test_explicit_imports
 using Test
 using P4est
 
 @testset "Aqua.jl" begin
     Aqua.test_all(P4est; unbound_args = false)
+end
+
+@testset "ExplicitImports.jl" begin
+    test_explicit_imports(P4est;
+                          # We use `MPI_Comm`, `MPI_Datatype`, `MPI_File`, and `MPI_Group`, which are non-public
+                          all_explicit_imports_are_public = false,
+                          # We use `MPIPreferences.binary`, which is non-public
+                          all_qualified_accesses_are_public = false,
+                          # We use `P4est.version()`
+                          no_self_qualified_accesses = false)
 end
 
 end #module
