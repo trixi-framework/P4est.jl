e6c9db38bcdf06aa2c58db5ba8d1956c8931d7ed
Joshua Lampert <joshua.lampert@uni-hamburg.de>
2026-09-18 12:17:10 +0200

    add ExplicitImport.jl tests

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -1,11 +1,16 @@
 module TestAqua
 
-using Aqua
+using Aqua: Aqua
+using ExplicitImports: test_explicit_imports
 using Test
 using P4est
 
 @testset "Aqua.jl" begin
     Aqua.test_all(P4est; unbound_args = false)
+end
+
+@testset "ExplicitImports.jl" begin
+   test_explicit_imports(P4est)
 end
 
 end #module
