3acc4433acf6b873bc0d0f3ca21d388aea8648b1
Joshua Lampert <joshua.lampert@uni-hamburg.de>
2026-09-24 12:22:39 +0200

    load P4est_jll only if preferences say so

--- dev/prologue.jl
+++ dev/prologue.jl
@@ -1,8 +1,9 @@
-using P4est_jll: P4est_jll
-export P4est_jll
-
 using ..P4est: _PREFERENCE_LIBP4EST, _PREFERENCE_LIBSC
 using MPIPreferences: MPIPreferences
+
+@static if _PREFERENCE_LIBP4EST == "P4est_jll" || _PREFERENCE_LIBSC == "P4est_jll"
+    using P4est_jll: P4est_jll
+end
 
 @static if _PREFERENCE_LIBP4EST == "P4est_jll" && MPIPreferences.binary == "system"
     @warn "System MPI version detected, but not a system p4est version. To make P4est.jl work, you need to set the preferences, see https://trixi-framework.github.io/P4est.jl/stable/#Using-a-custom-version-of-MPI-and/or-p4est."
